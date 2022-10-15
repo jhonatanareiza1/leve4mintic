622fb8202237865abf5fa0a39ba6aca5a1b7dc62
jhonatanareiza1 <31416701+jhonatanareiza1@users.noreply.github.com>
2022-10-15 12:34:10 -0500

    first commit

---
+++ loopback/proyecto1/src/models/pedido.model.ts
@@ -0,0 +1,57 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Pedido extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  id_producto: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cantidad: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  estado: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  total: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  id_persona: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Pedido>) {
+    super(data);
+  }
+}
+
+export interface PedidoRelations {
+  // describe navigational properties here
+}
+
+export type PedidoWithRelations = Pedido & PedidoRelations;
